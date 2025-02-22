812698eb4a9e62f3d06a7b07b9c8a028c1b2aff6
CarlosS13 <Carloscaprari36@gmail.com>
2025-02-22 01:56:38 -0300

    Entrega finalizada

--- script.js
+++ script.js
@@ -54,9 +54,11 @@ document.addEventListener("DOMContentLoaded", function(){
 // En esta segunda parte del codigo, realizo la funcionalidad de la agenda de turnos.
 let botones = document.querySelectorAll("#botonDia");
 let formulario = document.getElementById("containerDatos");
+let diaSeleccionado = null;
 
 botones.forEach(boton => {
     boton.addEventListener("click", () => {
+        diaSeleccionado = boton.textContent.trim(); 
         if (formulario.style.display === "none" || formulario.style.display === ""){
             formulario.style.display = "block";
         } else {
@@ -66,3 +68,75 @@ botones.forEach(boton => {
 });
 
 // // Dejo este comentario para el profe o tutor que este corrigiendo la entrega. Tuve una semana complicada en lo laboral, estoy dando soporte como auxiliar de supervisor en mi trabajo y he tenido muchas reuniones de capacitacion, por lo que me falto tiempo para finalizar la entrega. Un compañero del curso se habia comunicado antes para pedir la extension de la entrega y le dijeron que hable hoy mismo, hable con soporte pero no sabia que no hay mas asesores en linea en este horario (tengo capturas del chat). Si es posible y no hay problema, realizo la entrega igualmente, para que en la plataforma de coderhouse figure como entregada y en el transcurso de esta noche si lo llego a finalizar o mañana a la mañana agrego el contenido que me falta y lo actualizo dentro del repositorio con otro push. Perdon y muchas gracias! Saludos!
+
+let popup = document.getElementById("confirmacion");
+
+let turnos = JSON.parse(localStorage.getItem("turnos")) || [];
+
+let agregarTurno = document.getElementById("botonEnviar");
+
+let verificarTurno = function (nuevoTurno) {
+    let existente = false;
+    turnos.forEach(turno => {
+        if (turno.hora === nuevoTurno.hora && turno.dia === nuevoTurno.dia) {
+            existente = true;
+        }
+    });
+    return existente;
+};
+
+agregarTurno.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    let nombre = document.getElementById("inputNombre").value;
+    let apellido = document.getElementById("inputApellido").value;
+    let email = document.getElementById("inputCorreo").value;
+    let telefono = document.getElementById("inputTelefono").value;
+    let hora = document.getElementById("inputHora").value;
+
+    if (nombre === "" || apellido === "" || email === "" || telefono === "" || hora === "" || diaSeleccionado === null) {
+        alert("Completa todos los campos para finalizar");
+        return;
+    }
+
+    // Las validaciones para que el formato de correo y telefono sean correctos, lo tuve que googlear, no encontraba forma de hacerlo.
+     let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+     if (!emailPattern.test(email)) {
+         alert("Ingresa un correo electrónico válido");
+         return;
+     }
+     let telefonoPattern = /^\d{10,15}$/;
+     if (!telefonoPattern.test(telefono)) {
+         alert("Ingresa un número de teléfono válido");
+         return;
+     }
+
+    let nuevoTurno = {
+        dia: diaSeleccionado,
+        nombre: nombre,
+        apellido: apellido,
+        email: email,
+        telefono: telefono,
+        hora: hora,
+    };
+
+    if (verificarTurno(nuevoTurno)) {
+        alert("Ya hay un turno a esa hora en ese día, selecciona otro turno");
+        return;
+    }
+
+    popup.classList.add("activo");
+    setTimeout(() => { popup.classList.remove("activo"); }, 3500);
+
+    turnos.push(nuevoTurno);
+
+    localStorage.setItem("turnos", JSON.stringify(turnos));
+
+    formulario.style.display = "none";
+
+    document.getElementById("formDatos").reset();
+});
+
+// localStorage.removeItem("turnos");
+
+// Ahora si, entrega finalizada. Tengo una duda en cuanto al orden de mi forma de declarar las funciones, arranque haciendo el addeventlistener del domcontentloaded en ambos casos y si iba necesitando alguna funcion para invocar dentro del listener, la declaro encima y no despues. Esta bien o mejor declarar las funciones debajo? Gracias! Saludos!
